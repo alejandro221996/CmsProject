68d132b42a5627fe2c7ec00b0970824fa1f228c6
admin <admin@MBP13.local>
2023-03-12 15:46:16 -0600

    cambio en boton show/hide pass

--- SideProject/pages/auth/login.vue
+++ SideProject/pages/auth/login.vue
@@ -93,9 +93,15 @@
   color: #17b0eb;
 }
 
-.eye-button{
-  height: 47px;
-  width: 48 px;
+.hide-pass{
+  display: block;
+  margin: auto;
+  
+}
+.show-pass{
+  display: block;
+  margin: auto;
+  background: rgb(150, 150, 150);
 }
 </style>
 
@@ -135,18 +141,22 @@ export default {
     el: '#app',
     data(){
       return {
-        type: "password"
+        type: "password",
+        typeIcon: "hide-pass"
       };
     },
     props:{
-      type: {type:String, default:"password"}
+      type: {type:String, default:"password"},
+      typeIcon: {type:String, default:"hide-pass"}
     },
     methods:{
       showPassword() {
         if(this.type === 'password') {
           this.type = 'text'
+          this.typeIcon = 'show-pass'
         } else {
           this.type = 'password'
+          this.typeIcon = 'hide-pass'
         }
     }
 	}
@@ -211,12 +221,15 @@ definePageMeta({
                           name="password" placeholder="Password..."
                       />
                   <div class="w-12 h-24 flex items-center">
-                    <label type="button" @click="showPassword" :class="{
+                    <label 
+                        type="button" 
+                        @click="showPassword" 
+                        :class="{
                           'w-60 h-12  border-2 border-black outline-none text-xl font-thin mt-7': true,
                           }">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <svg :class="this.typeIcon" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                       </svg>
                     </label>
                   </div>
