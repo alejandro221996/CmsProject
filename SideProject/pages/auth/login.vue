c8f993e97305f6eaaa6e81f3370521dc71552f65
alejandro221996 <alejandro1996221@gmail.com>
2023-03-13 01:43:48 -0600

    Fix para width completo de show password

--- SideProject/pages/auth/login.vue
+++ SideProject/pages/auth/login.vue
@@ -225,7 +225,7 @@ definePageMeta({
                         type="button" 
                         @click="showPassword" 
                         :class="{
-                          'w-60 h-12 border-y-2 border-r-2 border-black outline-none text-xl font-thin mt-7': true,
+                          'w-full h-12 border-y-2 border-r-2 border-black outline-none text-xl font-thin mt-7': true,
                           }">
                       <svg :class="this.typeIcon" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
