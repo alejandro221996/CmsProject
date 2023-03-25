381a75b3b66e4fdfb4e92588e0c9b546f65613c6
admin <admin@MBP13.local>
2023-03-25 17:53:21 -0600

    Cambios en pages/configuration/posts/index, filtrar posts por usuarios y crear nuevos posts

--- SideProject/pages/configuration/posts/index.vue
+++ SideProject/pages/configuration/posts/index.vue
@@ -16,13 +16,66 @@
 </script >
 <template>
     <h1 class="text-3xl mb-4 font-mono font-bold">Managing posts</h1>
-        <section class="border-2 border-gray-300 rounded-lg p-10">
+
+    <!-- Seccion para editar, filtrar los posts creados por mi o por otros usuarios, crear o administrar posts. -->
+    <section class="flex justify-center items-center">
+        <div class="mr-1 mb-3 w-full border-2 border-gray-300 rounded-lg p-10">
+            <h2 class="mb-2">Filters</h2>
+            <FormKit
+                type="form"
+                id="searchUserForm"
+                submit-label="Search by"
+                :submit-attrs="{
+                  inputClass: 'w-32 h-10 bg-blue-500 text-white font-bold rounded-xl',
+                }"
+            >
+            <div class="flex items-center">
+                <FormKit label="User"
+                    :input-class="{
+                    'w-72 h-10 border-2 border-black outline-none text-xl font-thin px-2 rounded-xl mb-2': true,
+                    }"
+                    :label-class="{
+                        'text-xl':true
+                    }"
+                    type="text" name="username" placeholder="Search by user"
+                />
+            </div>
+                
+            </FormKit>
+        </div>
+        <div class="ml-1 mb-3 w-full border-2 border-gray-300 rounded-lg p-10">
+            <h2 class="mb-2">Create post</h2>
+            <FormKit
+                type="form"
+                id="createPostForm"
+                submit-label="Add"
+                :submit-attrs="{
+                  inputClass: 'w-32 h-10 bg-blue-500 text-white font-bold rounded-xl',
+                }"
+            >
+            <div class="flex items-center">
+                <FormKit label="Title"
+                    :input-class="{
+                    'w-72 h-10 border-2 border-black outline-none text-xl font-thin px-2 rounded-xl mb-2': true,
+                    }"
+                    :label-class="{
+                        'text-xl':true
+                    }"
+                    type="text" name="titlePost" placeholder="Title post"
+                />
+            </div>
+                
+            </FormKit>
+        </div>   
+    </section>
+
+        <main class="border-2 border-gray-300 rounded-lg p-10">
             <h1 class="text-2xl mb-10 font-mono font-semibold">Posts ({{ count }})</h1>
             <table class="w-full">
                 <thead>
                     <tr class="border-b-2 border-gray-300 text-left text-xl font-mono font-semibold">
                         <th class="p-4">Username</th>
-                        <th>Email</th>
+                        <th>Emaill</th>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Role</th>
@@ -53,5 +106,5 @@
                     </tr>
                 </tbody>
             </table>
-        </section>
+        </main>
 </template>
