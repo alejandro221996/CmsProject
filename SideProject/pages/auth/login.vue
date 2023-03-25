9fcd09ad2e6b52269cfbb5edc5b6cd9429d02526
admin <admin@MBP13.local>
2023-03-25 12:33:04 -0600

    Ajsutes en el boton de hide/show password, ajustes en inputs y labels en login y register

--- SideProject/pages/auth/login.vue
+++ SideProject/pages/auth/login.vue
@@ -93,15 +93,9 @@
   color: #17b0eb;
 }
 
-.hide-pass{
-  display: block;
-  margin: auto;
-  
-}
-.show-pass{
-  display: block;
-  margin: auto;
-  background: rgb(150, 150, 150);
+label{
+  margin-left: -40px;
+  cursor: pointer;
 }
 </style>
 
@@ -170,11 +164,11 @@ definePageMeta({
     <div class="login">
       <div class="login__container">   
         <div class="container_for_input">
-            <div class="login__container__logo">
-                <img src="~/assets/images/login.png" alt="logo" />
-            </div>
             <div class="container_for_input__title">
-                <h1>Sign in</h1>
+              <h1 class="text-3xl text-black font-mono">Sign in</h1>
+            </div>
+            <div class="login__container__logo">
+                <img class="w-32 h-32 " src="~/assets/images/login.png" alt="logo" />
             </div>
             <FormKit
                 type="form"
@@ -188,51 +182,59 @@ definePageMeta({
                 
                 }"
             >
-                <div class="w-full h-24 flex justify-center items-center">
+                <div class=" flex justify-center items-center">
                     <FormKit label="Username"
                         :input-class="{
-                        'w-72 h-12  border-2 border-black outline-none text-xl font-thin': true,
+                        'w-72 h-12 border-2 border-black outline-none text-xl font-thin px-2 rounded-xl pb-6 pt-6': true,
                         }"
                         :messages-class="{
                         'text-red-500': true,
                         }"
                         :label-class="{
-                            'text-xl':true
+                            'text-xl px-14':true
                         }"
                         validation="required"
                         validation-visibility="dirty"
                         type="text" name="username" placeholder="Username..."
                     />
                 </div>
-                <div class="w-full h-24 flex justify-center items-center" id="app">
+                <div class=" flex justify-center items-center" id="app">
                       <FormKit label="Password"
                           :input-class="{
-                          'w-60 h-12  border-2 border-black outline-none text-xl font-thin': true,
+                          'w-72 h-12 border-2 border-black outline-none text-xl font-thin px-2 rounded-xl pb-6 pt-6': true,
                           }"
                           :messages-class="{
                           'text-red-500 fixed': true,
                           }"
                           :label-class="{
-                              'text-xl':true
+                          'text-xl px-14':true
                           }"
                           :type="this.type"
                           validation="required"
                           validation-visibility="dirty"
                           name="password" placeholder="Password..."
                       />
-                  <div class="w-12 h-24 flex items-center">
-                    <label 
-                        type="button" 
-                        @click="showPassword" 
-                        :class="{
-                          'w-60 h-12  border-2 border-black outline-none text-xl font-thin mt-7': true,
-                          }">
-                      <svg :class="this.typeIcon" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                      </svg>
-                    </label>
-                  </div>
+                      <div class="w-1 h-24 flex items-center ">
+                        <label 
+                            type="button" 
+                            @click="showPassword" 
+                            :class="{
+                              'w-8 h-12 pt-2 outline-none text-xl font-thin mt-7': true,
+                              }">
+                          <div v-if="this.typeIcon === 'show-pass'">
+                            <svg :class="this.typeIcon" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                          </div>
+                          <div v-else>
+                            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"></path>
+                            </svg>
+                          </div>
+                        
+                        </label>
+                      </div>
                 </div>
             </FormKit>
             <div v-else class="completeLogin">User was created successfully 👍</div>
